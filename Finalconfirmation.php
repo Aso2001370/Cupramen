1b25fa96454fd1f48380d7a73ab0a1ba4dd928a8
Aso2001370 <2001370@s.asojuku.ac.jp>
2021-12-03 11:50:05 +0900

    ファイル追加完了 12/03 更新

--- Finalconfirmation.php
+++ Finalconfirmation.php
@@ -1,28 +1,42 @@
-<?php require 'header.php';?>
+<?php session_start();?>
+<?php require 'header.php';?>.
 <h1>まだ注文は確定してません!</h1>
-<p>住所はお間違えないでしょうか</p>
+<p>住所、メールアドレス、電話番号はお間違えないでしょうか?</p>
 
 <?php
 //セッションから取得（仮で2番にしている）
-
-    $id = 2;
+$id = $_SESSION['ab'];
+//$getid=$_SESSION['abcd'];
     $pdo = new PDO('mysql:host=mysql154.phy.lolipop.lan;
 dbname=LAA1291596-katougumi;charset=utf8',
         'LAA1291596',
         'katougumi');
 
-    $sql = $pdo->prepare('SELECT * from customer  WHERE customer_id=?');
-    $sql->execute([$id]);
-    foreach ($sql as $data) {
-        echo 'メールアドレス', '<input type="text" name="mail" value="', $data['mail'], '">';
-        echo '<br>';
-        echo '名前', '<input type="text" name="name" value="', $data['name'], '">';
-        echo '<br>';
-        echo '住所', '<input type="text" name="address" value="', $data['address'], '">';
-        echo '<br>';
-        echo '<button type="submit">編集</button>';
-    }
+$sql = $pdo->prepare('SELECT * from customer WHERE mail=?');
+$sql->execute(['ab']);
+//$sql->execute([$id]);
+
+//foreach ($_SESSION as $data) {
+//        echo 'メールアドレス', '<input type="text" name="mail" value="', $id['mail'], '">';
+//        echo '<br>';
+//        echo '名前', '<input type="text" name="name" value="', $id['name'], '">';
+//        echo '<br>';
+//         echo '住所', '<input type="text" name="address" value="', $id['address'], '">';
+//         echo '<br>';
+//         echo '電話番号', '<input type="text" name="tel" value="', $id['tel'], '">';
+//         echo '<br>';
+//    }
+//
+
+
+foreach ($id as $key => $id) {
+echo 'メールアドレス', '<input type="text" name="mail" value="', $id['mail'], '">';
+echo '<br>';
+
+}
 ?>
+
+
 
 <input type="button" onclick="location.href='END.php'" value="はい">
     <input type="button" onclick="location.href='home.php'" value="いいえ">
